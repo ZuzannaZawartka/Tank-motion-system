cec6ab5d7e3495107e127d4941878c6de491bf34
Zuziaxy <raf.zuzia7@gmail.com>
2021-11-06 12:25:54 +0100

    feat: tank drawing

---
+++ tank_motion.js
@@ -0,0 +1,32 @@
+class Coords {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
+function showTank(container, tank) {
+    const tankElement = document.createElement('img')
+    tankElement.style.position = 'absolute'
+    tankElement.style.left = tank.position.x + 'px'
+    tankElement.style.top = tank.position.y + 'px'
+
+    tankElement.style.width = tank.size.x + 'px'
+    tankElement.style.height = tank.size.y + 'px'
+
+    tankElement.src = 'tank_model.jpg'
+    container.appendChild(tankElement)
+}
+
+function init() {
+    const container = document.getElementsByClassName('container')[0]
+
+    const tank = {
+        size: new Coords(100, 100),
+        position: new Coords(300, 600)
+    }
+
+    showTank(container, tank)
+}
+
+init()
