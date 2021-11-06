15157d45b417db4ba38b51afab225c89fbc8af29
Zuziaxy <raf.zuzia7@gmail.com>
2021-11-06 15:04:42 +0100

    feat: added function tank rotation and tank movement

--- tank_motion.js
+++ tank_motion.js
@@ -17,20 +17,61 @@ function showTank(container, tank) {
     tankElement.style.height = tank.size.y + 'px'
 
     tankElement.src = tank.src
+
+    container.innerHTML = ''
     container.appendChild(tankElement)
+}
+
+function handleEvents(keymap, container, tank) {
+    window.addEventListener('keydown', event => {
+        switch(Object.keys(keymap).find(key => keymap[key].includes(event.key))) {
+            case 'RIGHT':
+                tank.rotation += tank.rotationSpeed
+            break
+            case 'LEFT':
+                tank.rotation -= tank.rotationSpeed
+            break
+            case 'UP':
+                moveTank(tank, -tank.speed)
+            break
+            case 'DOWN':
+                moveTank(tank, tank.speed)
+            break
+        }
+
+        showTank(container, tank)
+    })
+}
+
+function moveTank(tank, moveBy) {
+    const rad = tank.rotation * ( Math.PI / 180 )
+
+    tank.position = new Coords(
+        tank.position.x - moveBy * Math.sin(rad),
+        tank.position.y + moveBy * Math.cos(rad)
+    )
 }
 
 function init() {
     const container = document.getElementsByClassName('container')[0]
+    const keymap = {
+        UP: [ 'ArrowUp', 'w', 'W' ],
+        DOWN: [ 'ArrowDown', 's', 'S' ],
+        RIGHT: [ 'ArrowRight', 'd', 'D' ],
+        LEFT: [ 'ArrowLeft', 'a', 'A' ],
+    }
 
     const tank = {
         size: new Coords(100, 100),
         position: new Coords(300, 600),
-        rotation: 45,
+        speed: 5,
+        rotation: 0,
+        rotationSpeed: 5,
         src: 'tank_model.jpg'
     }
-
+    
     showTank(container, tank)
+    handleEvents(keymap, container, tank)
 }
 
 init()
